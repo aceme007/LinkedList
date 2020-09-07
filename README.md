7581d3ccdedf89ec1771624fff9b50865008f2ba
aceme007 <kushkaul@outlook.com>
2020-09-07 04:06:34 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LinkedList
+
+Linked Lists Python
---
+++ llist.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data=None):
+        #  --------------
+        # | data  | None |
+        #  --------------
+        self.data = data
+        self.next = None
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None # head points to NULL for empty LL
+
+    def printLinkedList(self):
+        current = self.head
+        print(f"{type(self).__name__} contents :")
+        while current != None:
+            print(f"{current.data}")
+            current = current.next
+        print("\n")
+
+    def insertNodeAtEnd(self, newNode: Node):
+        if self.head == None: # no node present in LL
+            self.head = newNode
+        else:
+            # traverse LL 1st
+            current = self.head
+            while current.next != None:
+                current = current.next
+            current.next = newNode
+
+
+if __name__ == "__main__":
+    ll = SingleLinkedList()
+    ll.insertNodeAtEnd(Node(data=10))
+    ll.printLinkedList()
+    ll.insertNodeAtEnd(Node(data=3))
+    ll.printLinkedList()
+    ll.insertNodeAtEnd(Node(data=33))
+    ll.printLinkedList()
